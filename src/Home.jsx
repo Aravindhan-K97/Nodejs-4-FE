1d8e5d3dfe831635546c85556c3e62352b381da3
Aravindhan-K97 <aravindhan2825@gmail.com>
2024-09-24 13:17:22 +0530

    Second commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,7 @@ export const Home = () => {
     <div>
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen flex flex-col justify-center items-center">
       <h1 className="text-Black text-6xl font-bold">Welcome Home!</h1>
-      <p className="text-Black text-2xl mt-4">This is a super good looking home component.</p>
-    </div>
+     </div>
     </div>
   )
 }
