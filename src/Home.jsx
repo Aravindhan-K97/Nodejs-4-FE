b48b78b9233b1be10736039eb73fcf8c1cbbde3c
Aravindhan-K97 <aravindhan2825@gmail.com>
2024-09-24 11:17:57 +0530

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export const Home = () => {
+  return (
+    <div>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-Black text-6xl font-bold">Welcome Home!</h1>
+      <p className="text-Black text-2xl mt-4">This is a super good looking home component.</p>
+    </div>
+    </div>
+  )
+}
